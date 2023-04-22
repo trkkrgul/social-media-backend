ec67483ac431b6484366e0e038b708d45fd0dd43
trkkrgul <109871858+trkkrgul@users.noreply.github.com>
2023-04-22 05:57:37 +0300

    Create deletePost.js

---
+++ src/controllers/deletePost.js
@@ -0,0 +1,28 @@
+import { Post, User } from "../models/index.js";
+
+const deletePost = async (req, res) => {
+  try {
+    const { walletAddress } = req.user;
+    const { postId } = req.params;
+
+    const user = await User.findOne({ walletAddress: walletAddress });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const post = Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    if (post.user.toString() !== user._id.toString()) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    res.status(200).json({ message: "Post deleted" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export default deletePost;
