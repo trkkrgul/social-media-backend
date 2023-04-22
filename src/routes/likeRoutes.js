069007f886cf3ecccb9cea1b79ae6eb42d507eb7
trkkrgul <109871858+trkkrgul@users.noreply.github.com>
2023-04-22 05:49:18 +0300

    Update likeRoutes.js

--- src/routes/likeRoutes.js
+++ src/routes/likeRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { likePost, dislikePost } from "../controllers/index.js";
+import authMiddleware from "../middleware/authValidator.js";
 
 const router = express.Router();
 
-router.post("/like", likePost);
-router.post("/dislike", dislikePost);
+router.post("/like", authMiddleware, likePost);
+router.post("/dislike", authMiddleware, dislikePost);
 
 export default router;
