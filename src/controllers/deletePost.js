fbf2f7f782818aa7b154853809c88c0a2e880b3b
trkkrgul <109871858+trkkrgul@users.noreply.github.com>
2023-04-23 11:50:56 +0300

    Update deletePost.js

--- src/controllers/deletePost.js
+++ src/controllers/deletePost.js
@@ -1,23 +1,40 @@
-import { Post, User } from "../models/index.js";
+import Post from "./models/Post"; // Make sure the path is correct
 
-const deletePost = async (req, res) => {
+async function deletePost(req, res) {
   try {
-    const { walletAddress } = req.user;
+    // Retrieve the user's wallet address from the middleware
+    const userWalletAddress = req.user.walletAddress;
+
+    // Retrieve the post ID from the request parameters
     const { postId } = req.body;
 
-    const user = await User.findOne({ walletAddress: walletAddress });
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
+    // Find the post by its ID and the user's wallet address
+    const post = await Post.findOne({
+      _id: postId,
+      user: userWalletAddress,
+    });
+
+    // If the post is not found, return a 404 error
+    if (!post) {
+      return res.status(404).json({
+        message: "Post not found or not owned by the user",
+      });
     }
 
-    const post = Post.findById(postId);
+    // Remove the post
+    await Post.deleteOne({ _id: postId });
 
-    const result = post.populate("user", "walletAddress");
-    console.log("owner", result.user.walletAddress);
-    res.status(200).json({ message: "Post deleted" });
+    // Return a success message
+    res.status(200).json({
+      message: "Post removed successfully",
+    });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    // Handle any errors and return a 500 error
+    console.error(error);
+    res.status(500).json({
+      message: "An error occurred while removing the post",
+    });
   }
-};
+}
 
 export default deletePost;
