d0c5017a3f9b9c43d94a855578804878043b0ae7
Barış Ertakuş <bariserta@gmail.com>
2023-05-10 10:06:10 +0200

    Add file limit and type filter

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -14,7 +14,25 @@ import multer, { memoryStorage } from "multer";
 const router = express.Router();
 
 const storage = memoryStorage();
-const uploadMw = multer({storage}).array('images', 10);
+const uploadMw = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype == "image/png" ||
+      file.mimetype == "image/jpg" ||
+      file.mimetype == "image/jpeg" ||
+      file.mimetype == "image/gif"
+    ) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+      return cb(new Error("Only .png, .jpg, .jpeg, .gif format allowed!"));
+    }
+  },
+  limits: {
+    fileSize: 5 * 1024 * 1024 // 5MB
+  }
+}).array("images", 10, {});
 
 router.post("/create", uploadMw, authMiddleware, createPost);
 router.get("/feed", getPosts); 
